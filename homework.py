a075b5f73d49c9cbd51b2b40e84eb926ea139b62
Renat <spamhatertom@gmail.com>
2023-01-06 13:35:11 +0300

    fast fix

--- homework.py
+++ homework.py
@@ -9,7 +9,7 @@ import requests
 import telegram
 from dotenv import load_dotenv
 
-from exceptions import (
+from exceptions import(
     GetAPICustomError,
     SendMessageCustomError,
     ParseStatusError,
